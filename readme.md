3f2605703c81207d75373cc9e09a7b8abddd4b8c
scottongithub <wherami@gmail.com>
2022-06-09 13:35:44 -0400

    Update OriginStamp API to v4

--- readme.md
+++ readme.md
@@ -2,8 +2,8 @@
 ## Purpose
 To sign and timestamp files, both directly with trusted timestamping authorities (TSAs) and OriginStamp, and indirectly by outputting-to-console both the (gpg-signed) file's sha256 hash and an email address for easy copy-paste into other services. Each file is turned into a folder archive with everything needed to recreate the chain from content to claim.
 
-## What it does
-It looks in the source directory and iterates on each file (with an extension):
+## How it Works
+It looks in the source directory and iterates on each file (that has an extension and no whitespace):
 - create an archive directory in the destination directory, named after the current file (e.g. "my_song.mp3" becomes a folder "my_song")
 - sign the file with the default GPG profile and save it to the archive folder
 - create a sha256 hash of the singed file and save it to the archive folder
@@ -11,3 +11,29 @@ It looks in the source directory and iterates on each file (with an extension):
 - for every TSA listed in tsa_to_url (associative array), submit the timestamp request to the current TSA, append the current TSA name to its reply-->archive
 - submit the sha256 hash of the signed file to OriginStamp via API and log the results in the .log file-->archive
 - source file gets moved to archive
+
+# Prerequisites
+- openssl
+- gnupg set up with default account
+- OriginStamp account/API key if this service is to be used. Uses API v4
+
+# Usage
+Configuration is the top lines of `stamper.sh`: email (optional), OriginStamp API key (optional), source/destination directories.
+
+Make it executable: `chmod +x stamper.sh`
+
+Run it: `./stamper.sh`. If all goes well, the archive folder (for a source file `my_song.mp3` will look something like this:
+```
+.
+..
+2991ADF7A1FA5A377ECAB7447F1E2500C615160DB6D73BD86EAF53B4ED26B5B7
+gpg_public_key.txt
+my_song_apple.tsr
+my_song_digicert.tsr
+my_song_free_tsa.tsr
+my_song.gpg
+my_song.log
+my_song.mp3.gpg
+my_song_safe_creative.tsr
+my_song.tsq
+```
--- stamper.sh
+++ stamper.sh
@@ -3,7 +3,7 @@
 # email address to be displayed in shell for copy/paste purposes
 email=""
 
-# originstamp API key
+# originstamp API key; leave empty to bypass
 originstamp_key=""
 
 source_dir=""
@@ -21,38 +21,38 @@ tsa_to_url[apple]="http://timestamp.apple.com/ts01"
 cd $source_dir
 for file in *.*; do
 
-  # strip file extension and store as variable 'name'
+  # strip file extension and store as 'name'
   name=$(echo "$file" | cut -f 1 -d '.')
   final_dest_dir=$dest_dir/$name
   if [[ ! -d $final_dest_dir ]]; then
-
-      mkdir $final_dest_dir;
+      mkdir $final_dest_dir
 
       gpg --sign $file
-
-      file_signed=$(ls -1 $name.*.gpg | head -n1);
+      file_signed=$(ls -1 $name.*.gpg | head -n1)
 
       # make a sha256 of it and store in a text file whose name is also the hash
-      sha256=$(sha256sum $file_signed | cut -f 1 -d ' ');
+      sha256=$(sha256sum $file_signed | cut -f 1 -d ' ')
       echo $sha256 > "$final_dest_dir/$sha256"
 
       # create a RFC 3161 time-stamp query
-      openssl ts -query -data $file_signed -no_nonce -sha512 -cert -out "$final_dest_dir/${name}.tsq";
+      openssl ts -query -data $file_signed -no_nonce -sha512 -cert -out "$final_dest_dir/${name}.tsq"
 
       # iterate on timestamp authorities and for each store the result in a file with the TA name appended
-      # with associative arrays, a ! first refers to keys and without ! refers to values
+      # with associative arrays here, a ! first refers to keys and without ! refers to values
       for timestamping_authority in "${!tsa_to_url[@]}"; do
         printf "\n$name\n${tsa_to_url[$timestamping_authority]}\n"
         curl -H "Content-Type: application/timestamp-query" \
           --data-binary "@${final_dest_dir}/${name}.tsq" "${tsa_to_url[@]}" > \
-          "$final_dest_dir/${name}_${timestamping_authority}.tsr";
+          "$final_dest_dir/${name}_${timestamping_authority}.tsr"
       done
 
       #post to originstamp via API
-      curl -X POST "http://api.originstamp.com/v3/timestamp/create"\
-           -H "Authorization: $originstamp_key"\
-           -H "Content-Type: application/json"\
-           -d '{"comment": "'"$name"'", "hash": "'"$sha256"'"}' | tee -a "$final_dest_dir/${name}.log";
+      curl -X POST "https://api.originstamp.com/v4/timestamp/create" \
+          -H "accept: application/json" \
+          -H "Authorization: $originstamp_key" \
+          -H "Content-Type: application/json" \
+          -d '{"comment": "'"$name"'", "hash": "'"$sha256"'", "notifications": [{"curreny": 0, "notification_type": 0, "target": "'"$email"'"}]}' \
+          | tee -a "$final_dest_dir/${name}.log"
 
   else
     echo "Looks like $file has already been done - please rename it if you'd like to stamp/archive it"
@@ -64,10 +64,11 @@ for file in *.*; do
   echo >> /tmp/stamper_report.txt
   mv $file "$final_dest_dir/$file"
   mv $file_signed "$final_dest_dir/$file_signed"
-  gpg --export -a > "$final_dest_dir/gpg_public_key.txt";
+  gpg --export -a > "$final_dest_dir/gpg_public_key.txt"
 done
 
 # prints hashes and email address for easier copy-pasting into non RFC 3161 stampers
+echo
 printf "\n$email\n\n"
 cat /tmp/stamper_report.txt
 rm /tmp/stamper_report.txt
